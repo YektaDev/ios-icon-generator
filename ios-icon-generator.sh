fac772998aff7d53b6cb29a36e59d8484d5151fa
Ali Khaleqi Yekta <Me@Yekta.Dev>
2024-02-06 16:05:13 +0330

    BRING BACK TO LIFE!

--- ios-icon-generator.sh
+++ ios-icon-generator.sh
@@ -1,81 +1,61 @@
 #!/bin/bash
-#
-# Copyright (C) 2018 smallmuou <smallmuou@163.com>
-# 
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is furnished
-# to do so, subject to the following conditions:
-# 
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-# 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
 
 set -e
 
 spushd() {
-     pushd "$1" 2>&1> /dev/null
+  pushd "$1" > /dev/null 2>&1
 }
 
 spopd() {
-     popd 2>&1> /dev/null
+  popd > /dev/null 2>&1
 }
 
 info() {
-     local green="\033[1;32m"
-     local normal="\033[0m"
-     echo -e "[${green}INFO${normal}] $1"
+  local green="\033[1;32m"
+  local normal="\033[0m"
+  echo -e "[${green}INFO${normal}] $1"
 }
 
 cmdcheck() {
-    command -v $1>/dev/null 2>&1 || { error >&2 "Please install command $1 first."; exit 1; }   
+  command -v $1>/dev/null 2>&1 || { error >&2 "Please install command $1 first."; exit 1; }
 }
 
 error() {
-     local red="\033[1;31m"
-     local normal="\033[0m"
-     echo -e "[${red}ERROR${normal}] $1"
+  local red="\033[1;31m"
+  local normal="\033[0m"
+  echo -e "[${red}ERROR${normal}] $1"
 }
 
 warn() {
-     local yellow="\033[1;33m"
-     local normal="\033[0m"
-     echo -e "[${yellow}WARNING${normal}] $1"
+  local yellow="\033[1;33m"
+  local normal="\033[0m"
+  echo -e "[${yellow}WARNING${normal}] $1"
 }
 
 yesno() {
-    while true;do
-    read -p "$1 (y/n)" yn
+  while true;do
+    read -rp "$1 (y/n)" yn
     case $yn in
-        [Yy]) $2;break;;
-        [Nn]) exit;;
-        *) echo 'please enter y or n.'
+      [Yy]) $2;break;;
+      [Nn]) exit;;
+      *) echo 'Please enter y or n.'
     esac
-done
+  done
 }
 
 curdir() {
-    if [ ${0:0:1} = '/' ] || [ ${0:0:1} = '~' ]; then
-        echo "$(dirname $0)"
-    elif [ -L $0 ];then
-        name=`readlink $0`
-        echo $(dirname $name)
-    else
-        echo "`pwd`/$(dirname $0)"
-    fi
+  if [ ${0:0:1} = '/' ] || [ ${0:0:1} = '~' ]; then
+    dirname "$0"
+  elif [ -L "$0" ];then
+    name=$(readlink "$0")
+    dirname "${name}"
+  else
+    echo "$(pwd)/$(dirname "$0")"
+  fi
 }
 
 myos() {
-    echo `uname|tr "[:upper:]" "[:lower:]"`
+  echo $(uname|tr "[:upper:]" "[:lower:]")
 }
 
 #########################################
@@ -90,7 +70,7 @@ AUTHOR=smallmuou
 #########################################
 
 usage() {
-prog=`basename $0`
+prog=$(basename "$0")
 cat << EOF
 $prog version $VERSION by $AUTHOR
 
@@ -113,15 +93,15 @@ exit 1
 }
 
 while getopts 'h' arg; do
-    case $arg in
-        h)
-            usage
-            ;;
-        ?)
-            # OPTARG
-            usage
-            ;;
-    esac
+  case $arg in
+    h)
+      usage
+      ;;
+    ?)
+      # OPTARG
+      usage
+      ;;
+  esac
 done
 
 shift $(($OPTIND - 1))
@@ -136,62 +116,60 @@ cmdcheck sips
 src_file=$1
 dst_path=$2
 
-# check source file
-[ ! -f "$src_file" ] && { error "The source file $src_file does not exist, please check it."; exit -1; }
+# Check source file
+[ ! -f "$src_file" ] && { error "The source file $src_file does not exist, please check it."; exit 1; }
 
-# check width and height 
-src_width=`sips -g pixelWidth $src_file 2>/dev/null|awk '/pixelWidth:/{print $NF}'`
-src_height=`sips -g pixelHeight $src_file 2>/dev/null|awk '/pixelHeight:/{print $NF}'`
+# Check width and height
+src_width=$(sips -g pixelWidth "$src_file" 2>/dev/null|awk '/pixelWidth:/{print $NF}')
+src_height=$(sips -g pixelHeight "$src_file 2>/dev"/null|awk '/pixelHeight:/{print $NF}')
 
-[ -z "$src_width" ] &&  { error "The source file $src_file is not a image file, please check it."; exit -1; }
+[ -z "$src_width" ] &&  { error "The source file $src_file is not a image file, please check it."; exit 1; }
 
-if [ $src_width -ne $src_height ];then
-    warn "The height and width of the source image are different, will cause image deformation."
+if [ "${src_width}" -ne "${src_height}" ];then
+  warn "The height and width of the source image are different, will cause image deformation."
 fi
 
-# create dst directory 
+# create dst directory
 [ ! -d "$dst_path" ] && mkdir -p "$dst_path"
 
-# ios sizes refer to https://developer.apple.com/design/human-interface-guidelines/ios/icons-and-images/app-icon/
-# macos sizes refer to https://developer.apple.com/design/human-interface-guidelines/macos/icons-and-images/app-icon/
-# watchos sizes refer to https://developer.apple.com/design/human-interface-guidelines/watchos/icons-and-images/home-screen-icons/
-# 
-# 
-# name size
-sizes_mapper=`cat << EOF
-Icon-16         16
-Icon-16@2x      32
-Icon-32         32
-Icon-32@2x      64
-Icon-128        128
-Icon-128@2x     256
-Icon-256        256
-Icon-256@2x     512
-Icon-512        512
-Icon-512@2x     1024
-Icon-20         20
-Icon-20@2x      40
-Icon-20@3x      60
-Icon-29         29
-Icon-29@2x      58
-Icon-29@3x      87
-Icon-40         40
-Icon-40@2x      80
-Icon-40@3x      120
-Icon-60@2x      120
-Icon-60@3x      180
-Icon-76         76
-Icon-76@2x      152
-Icon-83.5@2x    167
-Icon-1024       1024
-Icon-24@2x      48
-Icon-27.5@2x    55
-Icon-86@2x      172
-Icon-98@2x      196
-Icon-108@2x     216
-Icon-44@2x      88
-Icon-50@2x      100
-EOF`
+# iOS sizes refer to https://developer.apple.com/design/human-interface-guidelines/ios/icons-and-images/app-icon/
+# macOS sizes refer to https://developer.apple.com/design/human-interface-guidelines/macos/icons-and-images/app-icon/
+# watchOS sizes refer to https://developer.apple.com/design/human-interface-guidelines/watchos/icons-and-images/home-screen-icons/
+#
+# name  size
+sizes_mapper=\
+"icon_16       16
+icon_16@2x    32
+icon_32       32
+icon_32@2x    64
+icon_128      128
+icon_128@2x   256
+icon_256      256
+icon_256@2x   512
+icon_512      512
+icon_512@2x   1024
+icon_20       20
+icon_20@2x    40
+icon_20@3x    60
+icon_29       29
+icon_29@2x    58
+icon_29@3x    87
+icon_40       40
+icon_40@2x    80
+icon_40@3x    120
+icon_60@2x    120
+icon_60@3x    180
+icon_76       76
+icon_76@2x    152
+icon_83.5@2x  167
+icon_1024     1024
+icon_24@2x    48
+icon_27.5@2x  55
+icon_86@2x    172
+icon_98@2x    196
+icon_108@2x   216
+icon_44@2x    88
+icon_50@2x    100"
 
 OLD_IFS=$IFS
 IFS=$'\n'
@@ -199,17 +177,17 @@ srgb_profile='/System/Library/ColorSync/Profiles/sRGB Profile.icc'
 
 for line in $sizes_mapper
 do
-    name=`echo $line|awk '{print $1}'`
-    size=`echo $line|awk '{print $2}'`
-    info "Generate $name.png ..."
-    if [ -f $srgb_profile ];then
-        sips --matchTo '/System/Library/ColorSync/Profiles/sRGB Profile.icc' -z $size $size $src_file --out $dst_path/$name.png >/dev/null 2>&1
-    else
-        sips -z $size $size $src_file --out $dst_path/$name.png >/dev/null
-    fi
+  name=$(echo $line|awk '{print $1}')
+  size=$(echo $line|awk '{print $2}')
+  info "Generating: $name.png"
+  out="${dst_path}/${name}.png"
+  if [ -f "${srgb_profile}" ];then
+    sips --matchTo "${srgb_profile}" -z "${size}" "${size}" "${src_file}" --out "${out}" > /dev/null 2>&1
+  else
+    sips -z "${size}" "${size}" "${src_file}" --out "${out}" > /dev/null
+  fi
 done
 
-info "Congratulation. All icons for iOS/macOS/watchOS APP are generate to the directory: $dst_path."
+info "Congratulations! All icons for iOS/macOS/watchOS APP are generate to the directory: $dst_path"
 
 IFS=$OLD_IFS
-
